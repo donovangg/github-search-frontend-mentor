deda1ad909895b99eef86b0bf3c440ec77e5bafa
Donovan <donovanjuniogomez@gmail.com>
2023-08-30 01:44:32 -0700

    add search inputs

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import GithubForm from "./GithubForm";
 import UserCard from "./UserCard";
 
 const Card = () => {
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const searchUser = async (query: any) => {
+    try {
+      const res = await fetch(`https://api.github.com/users/${query}`);
+      const data = await res.json();
+      setUser(data);
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <Navbar />
-      <GithubForm />
+      <GithubForm user={user} setUser={setUser} searchUser={searchUser} />
       <UserCard />
     </div>
   );
--- src/app/components/GithubForm.tsx
+++ src/app/components/GithubForm.tsx
@@ -1,8 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const GithubForm = () => {
+type FormProps = {
+  user: string;
+  setUser: React.Dispatch<React.SetStateAction<string>>;
+  searchUser: (query: any) => Promise<void>;
+};
+
+const GithubForm = ({ user, setUser, searchUser }: FormProps) => {
+  const [queryHandler, setQueryHandler] = useState("");
+
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+    searchUser(queryHandler);
+    setQueryHandler("");
+  };
+
   return (
     <div className="w-80 border-2 border-pink-500">
       <form className="w-full border-2 border-orange-400">
@@ -10,8 +24,13 @@ const GithubForm = () => {
           <span>
             <img src="/assets/icon-search.svg" alt="search icon" />
           </span>
-          <input className="w-full" type="text" />
-          <button>Search</button>
+          <input
+            className="w-full"
+            type="text"
+            value={queryHandler}
+            onChange={(e) => setQueryHandler(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
         </div>
       </form>
     </div>
