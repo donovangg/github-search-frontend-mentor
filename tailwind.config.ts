afeaab6d8a57944e965e2a592a014fbec4ad588f
Donovan <donovanjuniogomez@gmail.com>
2023-09-08 01:18:23 -0700

    update tailwind config colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "main-blue": "#0079FF",
+        "black-logo": "222731",
+        "light-gray": "#F6F8FF",
+        "light-text": "#4B6A9B",
+        "light-mode-number": "#2B3442",
+        "dark-mode-bg": "#1E2A47",
+        "really-dark-bg": "#141D2F",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
