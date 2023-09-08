6ca9a5c332ee047bb8e072873d907bd165911f1e
Donovan <donovanjuniogomez@gmail.com>
2023-09-08 01:31:20 -0700

    change default Card to Octocat per ReadMe

--- src/app/components/DefaultCard.tsx
+++ src/app/components/DefaultCard.tsx
@@ -7,33 +7,35 @@ const DefaultCard = () => {
         <div>
           <img
             className="w-16 h-16 rounded-full "
-            src="/assets/hifumi.jpg"
+            src="/assets/octocat.png"
             alt="avatar"
           />
         </div>
         <div>
-          <h3>Takimoto Hifumi</h3>
-          <a href="https://github.com/TakimotoHifumin" target="_blank">
-            @TakimotoHifumin
+          <h3>Octocat</h3>
+          <a href="https://github.com/github" target="_blank">
+            @Octocat
           </a>
-          <p>Joined 22 December 2015</p>
+          <p>Joined 25 Jan 2011</p>
         </div>
       </div>
       <div className="w-60 border-red-500 mx-auto border-2">
-        「まんがタイムきららキャラット」にて連載中の大人気4コマ『NEW
-        GAME!』のTVアニメ公式ツイッターです
+        How people build software.
       </div>
       <div className="w-60 border-purple-500 border-2 mx-auto">
         <div className="flex gap-2 justify-center border-2 border-slate-600">
           <div className="border-2 border-pink-400 text-center">
             <h3>Repos</h3>
-            <p>28</p>
+            <p>8</p>
           </div>
           <div className="border-2 border-pink-400 text-center">
             <h3>Followers</h3>
             <p>
-              <a href="#" target="_blank">
-                129k
+              <a
+                href="https://github.com/orgs/github/followers"
+                target="_blank"
+              >
+                28.5k
               </a>
             </p>
           </div>
@@ -41,7 +43,7 @@ const DefaultCard = () => {
             <h3>Following</h3>
             <p>
               <a href="#" target="_blank">
-                24
+                9
               </a>
             </p>
           </div>
@@ -55,7 +57,7 @@ const DefaultCard = () => {
               src="/assets/icon-location.svg"
               alt="location"
             />
-            <p>Tokyo, Japan</p>
+            <p>San Francisco</p>
           </li>
 
           <li className="flex gap-2">
@@ -64,18 +66,22 @@ const DefaultCard = () => {
               src="/assets/icon-website.svg"
               alt="website"
             />
-            <a href="http://newgame-anime.com/" target="_blank">
-              http://newgame-anime.com/
+            <a href="https://github.blog/" target="_blank">
+              https://github.blog/
             </a>
           </li>
           <li className="flex gap-2">
             <img
-              className="w-5 h-5"
+              className="w-5 h-5 opacity-25"
               src="/assets/icon-twitter.svg"
               alt="twitter"
             />
-            <a href="https://twitter.com/nganime" target="_blank">
-              @nganime
+            <a
+              href="https://twitter.com/github"
+              className="opacity-25"
+              target="_blank"
+            >
+              Not Available
             </a>
           </li>
           <li className="flex gap-2">
@@ -88,7 +94,7 @@ const DefaultCard = () => {
               href="https://new-game.fandom.com/wiki/Eagle_Jump"
               target="_blank"
             >
-              Eagle Jump
+              GitHub
             </a>
           </li>
         </ul>
